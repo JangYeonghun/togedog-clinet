34ec807781bc2110107968bf7db6aa6c4ee65ea1
3C37 <dooho0421@gmail.com>
2024-09-07 20:06:14 +0900

    [ref] Refactor API repositories

---
+++ lib/src/repository/user_profile_repository.dart
@@ -0,0 +1,24 @@
+import 'package:dog/src/interface/api.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class UserProfileRepository extends API {
+
+  Future<Response> getProfile({
+    required BuildContext context
+  }) async {
+    return await storage.read(key: 'accessToken').then((accessToken) async {
+      return await api(
+          context: context,
+          func: () => get(
+              Uri.http('$domain:$port', '/api/v1/mate'),
+              headers: <String, String>{
+                'Content-type' : 'application/json',
+                'Authorization' : 'Bearer $accessToken'
+              }
+          )
+      );
+    });
+  }
+
+}
